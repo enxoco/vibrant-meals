94dc3dbe0d92408618b826f8441f1ee75a990a86
Mike Conrad <mike@enxo.co>
2019-01-25 14:23:33 -0500

    Updated routes

--- start/hooks.js
+++ start/hooks.js
@@ -1,17 +1,22 @@
 const { hooks } = require('@adonisjs/ignitor')
 const moment = require('moment')
-
+require('moment-countdown');
 
 hooks.after.providersBooted(() => {
   const View = use('View')
 
   View.global('convertTime', function (time) {
 
-    // Remove milliseconds from our time object
-      var t = time.split('.')[0]
+    var year = moment().format("YYYY-MM-DD")
+    var dateString = year + " " + time
+    var timeToClosing = moment(dateString.toString()).countdown()
+    console.log(dateString.toString())
+    if (timeToClosing.hours == 0) {
+      return this.safe('<a style="color:red;">'+ timeToClosing +'</a>')
+    } else {
+      return this.safe('<a style="color:green;">'+ timeToClosing +'</a>')
 
-      // convert 24hour to 12 hour
-      return moment(t, ["HH:mm:ss"]).format("h:mm A")
+    }
   })
 
   View.global('currency', function(num) {
--- start/routes.js
+++ start/routes.js
@@ -30,12 +30,19 @@ Route.post('/admin/categories/list/:cat_id', 'CategoryController.listItems')
 Route.get('/admin/orders', 'OrderController.viewOrdersAdmin')
 
 
+/**
+ * 
+ * This route now has to trigger part two of user registration.
+ * Now we need to capture 
+ */
 Route.get('/checkout', 'CheckoutController.startCheckout')
 Route.post('/cart/addItem', 'ItemController.addToCart')
 Route.get('/cart/clear', 'ItemController.clearCart')
 Route.get('/cart/sub/:cartPos', 'ItemController.subCart')
 Route.get('/cart/remove/:cartPos', 'ItemController.removeItem')
-Route.get('/', 'HomeController.index').as('welcomePage')
+Route.get('/', 'ItemController.showMenu')
+
+// Route.get('/', 'HomeController.index').as('welcomePage')
 Route.get('/menu/all', 'ItemController.showMenu')
 Route.get('/items/category/:cat_id', 'ItemController.list')
 Route.get('/orders/test', 'Auth/AuthController.testOrder')
@@ -45,6 +52,7 @@ Route.post('/register/:reg_method', 'Auth/AuthController.updateCustomerAddress')
 Route.get('/item/delete/:itemId', 'ItemController.deleteItem')
 Route.get('/item/hide/:itemId', 'ItemController.hideItem')
 
+Route.get('/api/stores/all', 'HomeController.showStores')
 
 Route.get('/item/add/:itemId', 'ItemController.editItem')
 Route.post('/item/add/:itemId', 'ItemController.updateItem')
@@ -59,7 +67,13 @@ Route.get('/logout', 'Auth/AuthController.logout').as('logout')
 
 Route.get('/manage/deliveries/:zip/', 'Auth/AuthController.showDeliveryOptions')
 Route.get('/manage/pickups/:zip/:radius', 'Auth/AuthController.showPickupOptions')
-Route.post('/register', 'Auth/AuthController.postGuestRegistration').as('register.store')
+/**
+ * 
+ * This will be the first point of contact with the customer
+ * We need to get their preference for pickup/delivery and save that to the session.
+ * Then we need to show them a modal asking for name and zip.
+ */
+Route.post('/register', 'Auth/AuthController.postGuestRegistration')
 
 Route.get('/password/reset', 'Auth/PasswordController.showResetForm').as('reset.form')
 Route.post('/password/email', 'Auth/PasswordController.sendResetLinkEmail').as('send.reset.email')
