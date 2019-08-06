6f618416637bc5952b7b30e5d1ff63a25c9cb57f
Mike Conrad <mike@enxo.co>
2019-08-06 11:11:53 -0400

    Make sure fulfillment date is never undefined

--- public/js/update-cart.js
+++ public/js/update-cart.js
@@ -96,7 +96,7 @@ function updateCartDiv(fulfillment_method) {
     }
     if (localStorage.fulfillment_day) {
       $('#fulfillment-date').val(localStorage.fulfillment_day.charAt(0).toUpperCase() + localStorage.fulfillment_day.slice(1) + ' - ' + localStorage.fulfillment_date)
-      $('.delivery-date-label, #delivery-date-label').html('<strong>'+capitalize(localStorage.fulfillment_method) + ' Date</strong> <br /><a href="#" id="update-fulfillment-day">' + localStorage.fulfillment_day.charAt(0).toUpperCase() + localStorage.fulfillment_day.slice(1) + ' - ' + localStorage.fulfillment_date + '</strong></a>')
+      $('.delivery-date-label, #delivery-date-label').html('<strong>'+capitalize(localStorage.fulfillment_method) + ' Date</strong> <br /><a href="#" id="update-fulfillment-day">' + localStorage.fulfillment_day.charAt(0).toUpperCase() + localStorage.fulfillment_day.slice(1) + ' - ' + $(".list-group-item.clickable.active").data("date") + '</strong></a>')
 
 
     }
